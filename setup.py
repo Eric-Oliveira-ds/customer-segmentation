1a75908b97118186d1990594e9d9c6716fae0d39
Eric Oliveira <eric.oliveira.pro@gmail.com>
2023-05-10 11:51:10 -0300

    fisrt commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='develop a customer segmentation to define the best marketing strategy',
+    author='Eric Oliveira',
+    license='MIT',
+)
